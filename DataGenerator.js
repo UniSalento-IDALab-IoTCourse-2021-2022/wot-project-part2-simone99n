9ad073900a91f37da660fcd11b01e240aa62333b
simone <simone.norberti@gmail.com>
2022-12-04 18:22:30 +0100

    [v0.2] sensor data simulator + mqtt connection

--- DataGenerator.js
+++ DataGenerator.js
@@ -1,25 +1,24 @@
-console.log(`\nSarcopenia project - Raspberry MQTT - Node.js v${process.versions.node}!`)
+console.log(`\nSARCOPENIA PROJECT - Raspberry MQTT - Norberti Simone - Node.js v${process.versions.node}!\n`)
 
 const mqtt = require('mqtt')
-const argv = require('minimist')(process.argv.slice(2));
-console.dir(argv); console.log();
 
-let patient = {
-    name : null,
-    surname : null,
-    sex : null,
-    height : null
-};
-let bia = {
-    FM : null,
-    FFM : null
-};
-let acceleration = {
-    acceleration_x : null,
-    acceleration_y : null,
-    acceleration_z : null
-}; // m/s^2
-let muscle_strenght = null; //grams
+const argv = process.argv
+let patient = 1 //default patient 1
+let anomaly = false //default no anomaly
+
+if (argv[2] === '--patient1' || argv[3] === '--patient1' )
+    patient = 1
+if (argv[2] === '--patient2' || argv[3] === '--patient2' )
+    patient = 2
+if (argv[2] === '--patient3' || argv[3] === '--patient3' )
+    patient = 3
+if (argv[2] === '--anomaly' || argv[3] === '--anomaly' )
+    anomaly = true
+console.log("patient = " + patient)
+console.log("anomaly = " + anomaly + "\n")
+//TODO implement 3 different patient
+//TODO implement anomalies
+
 const delay = 2000 //ms
 const clientId = 'mqtt_unisalento_sarcopenia_s'
 const connectUrl = `mqtt://mqtt.eclipseprojects.io:1883`
@@ -41,70 +40,70 @@ const option_publish = {
 
 const sleep = (ms) => {
     const startPoint = new Date().getTime()
-    while (new Date().getTime() - startPoint <= ms) {/* wait */}
+    while (new Date().getTime() - startPoint <= ms) {/* wait */
+    }
 }
 
 /*
 Here I introduce a method that can be used to generate random numbers drawn from a normal distribution.
 The code below uses the Box-Muller transform to make sure the numbers are normally distributed.
 */
+
 // metodo per generare coppie di numeri casuali indipendenti e distribuiti gaussianamente con media nulla e varianza uno
 function boxMullerTransform() {
     const u1 = Math.random();
     const u2 = Math.random();
     const z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(2.0 * Math.PI * u2);
     const z1 = Math.sqrt(-2.0 * Math.log(u1)) * Math.sin(2.0 * Math.PI * u2);
-    return { z0, z1 };
+    return {z0, z1};
 }
 
 //mapping (0,1) --> (mean, stddev)
 function getNormallyDistributedRandomNumber(mean, stddev) {
-    const { z0, _ } = boxMullerTransform();
+    const {z0, _} = boxMullerTransform();
     return z0 * stddev + mean
 }
 
 /* the following functions generate sensors data (bia/gaitSpeed/muscleStrenght) */
 
 function biaMeasureGenerator(meanFM, meanFFM, stddevFM, stddevFFM) {
-    bia.FM = getNormallyDistributedRandomNumber(meanFM, stddevFM)
-    bia.FFM = getNormallyDistributedRandomNumber(meanFFM, stddevFFM)
-    console.log("FM = " + bia.FM)
-    console.log("FFM = " + bia.FFM)
-    return `{ "bia_FM" : ${bia.FM}, "bia_FFM" : ${bia.FFM} }`
+    let FM = getNormallyDistributedRandomNumber(meanFM, stddevFM)
+    let FFM = getNormallyDistributedRandomNumber(meanFFM, stddevFFM)
+    console.log("FM = " + FM)
+    console.log("FFM = " + FFM)
+    return `{ "bia_FM" : ${FM}, "bia_FFM" : ${FFM} }`
 }
 
 function gaitSpeedMeasureGenerator(mean_X, mean_Y, mean_Z, stddev_X, stddev_Y, stddev_Z) {
-    acceleration.acceleration_x = getNormallyDistributedRandomNumber(mean_X, stddev_X)
-    acceleration.acceleration_y = getNormallyDistributedRandomNumber(mean_Y, stddev_Y)
-    acceleration.acceleration_z = getNormallyDistributedRandomNumber(mean_Z, stddev_Z)
-    console.log("acc_x = " + acceleration.acceleration_x)
-    console.log("acc_y = " + acceleration.acceleration_y)
-    console.log("acc_z = " + acceleration.acceleration_z)
-    return `{ "acc_x" : ${acceleration.acceleration_x}, 
-    "acc_y" : ${acceleration.acceleration_y},
-    "acc_z" : ${acceleration.acceleration_z} }`
+    let acceleration_x = getNormallyDistributedRandomNumber(mean_X, stddev_X)
+    let acceleration_y = getNormallyDistributedRandomNumber(mean_Y, stddev_Y)
+    let acceleration_z = getNormallyDistributedRandomNumber(mean_Z, stddev_Z)
+    console.log("acc_x = " + acceleration_x)
+    console.log("acc_y = " + acceleration_y)
+    console.log("acc_z = " + acceleration_z)
+    return `{ "acc_x" : ${acceleration_x}, 
+    "acc_y" : ${acceleration_y},
+    "acc_z" : ${acceleration_z} }`
 }
 
-function muscleStrenghtMeasureGenerator(mean_MS, stddev_MS){
-    muscle_strenght = getNormallyDistributedRandomNumber(mean_MS, stddev_MS)
-    console.log("muscle_strenght = " + muscle_strenght)
+function muscleStrenghtMeasureGenerator(mean_MS, stddev_MS) {
+    let muscle_strenght = getNormallyDistributedRandomNumber(mean_MS, stddev_MS)
+    console.log("muscle_strenght = " + muscle_strenght + "\n")
     return `{ "muscle_strenght" : ${muscle_strenght} }`
 }
 
 // ----------------------------------MQTT DATA SEND---------------------------------------------------------------------
-
-
 const client = mqtt.connect(connectUrl, option_connect)
 
-client.on("connect",function(){
-    console.log("Connected");
+client.on("connect", function () {
+    console.log("[MQTT] Connected");
 });
-client.on("error",function(error){
+client.on("error", function (error) {
     console.log("Can't connect" + error);
 });
 
 setInterval(function () {
-        client.publish(topic_bia, biaMeasureGenerator(20,50,1, 1) , option_publish, (error) => {
+        client.publish(topic_bia, biaMeasureGenerator(20, 50, 1, 1), option_publish, (error) => {
             if (error) {
                 console.error(error)
             }
@@ -113,7 +112,7 @@ setInterval(function () {
 );
 
 setInterval(function () {
-        client.publish(topic_acceleration, gaitSpeedMeasureGenerator(1.2 , 0.1, 0.1, 0.5, 0.05, 0.05), option_publish, (error) => {
+        client.publish(topic_acceleration, gaitSpeedMeasureGenerator(1.2, 0.1, 0.1, 0.5, 0.05, 0.05), option_publish, (error) => {
             if (error) {
                 console.error(error)
             }
@@ -122,7 +121,7 @@ setInterval(function () {
 );
 
 setInterval(function () {
-        client.publish(topic_musclestrenght, muscleStrenghtMeasureGenerator(10 , 1) , option_publish, (error) => {
+        client.publish(topic_musclestrenght, muscleStrenghtMeasureGenerator(10, 1), option_publish, (error) => {
             if (error) {
                 console.error(error)
             }
